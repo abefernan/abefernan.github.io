e97cc71d146a2b51ab3e452d4b651d07ecffaafb
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-20 22:23:13 +0200

    Binds htm to react in order to use hooks (preact hooks not available)

--- projects/wasspord/src/unpkg/htm.js
+++ projects/wasspord/src/unpkg/htm.js
@@ -1,1 +1,4 @@
-export * from "https://unpkg.com/htm@2.2.1/preact/standalone.module.js";
+import htm from 'https://unpkg.com/htm@2.2.1/dist/htm.module.js?module';
+import { React } from './react.js';
+
+export const html = htm.bind(React.createElement);
