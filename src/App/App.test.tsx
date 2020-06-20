9b0eaab410b2bcacb713dc92f4f990fcbf025f09
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-06-20 21:33:17 +0200

    Fixes wrong test name

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import * as React from 'react';
 import App from '.';
 
-test('renders learn react link', () => {
+test('renders check game link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/check out my game/i);
   expect(linkElement).toBeInTheDocument();
