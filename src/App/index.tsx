fafdf3b67bf37c5de180f1f245ace1fb465a292f
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-06-20 21:33:53 +0200

    Simplifies layout and styles

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,18 +5,14 @@ import davidBust from './assets/davidBust.png';
 function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={davidBust} className="App-logo" alt="David Bust" />
-        <p>Check out my game:</p>
-        <a
-          className="App-link"
-          href="https://abefernan.github.io/projects/wasspord/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h1>abefernan&apos;s site</h1>
+      <img src={davidBust} alt="David Bust" />
+      <h2>
+        Check out my game&nbsp;
+        <a href="https://abefernan.github.io/projects/wasspord/" target="_blank" rel="noopener noreferrer">
           WassPord
         </a>
-      </header>
+      </h2>
     </div>
   );
 }
