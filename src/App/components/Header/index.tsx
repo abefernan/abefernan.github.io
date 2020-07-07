265c1c78074259ee71071b33fc9d295e0098196b
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-07-07 12:45:58 +0200

    New design for Header

---
+++ src/App/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Header.css';
+
+function Header(): JSX.Element {
+  return (
+    <header className="Header">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">
+              <h1>@abefernan</h1>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/abefernan" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/abefernan/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li>
+            <a href="#whoami">Who am I?</a>
+          </li>
+          <li>
+            <a href="#whatdoido">What do I do?</a>
+          </li>
+          <li>
+            <a href="#whathaveidone">What have I done?</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
