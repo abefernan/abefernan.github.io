236ef64456eb19c145badc1bcb797b29e233ad46
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-07-07 12:45:49 +0200

    New design for App

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import './App.css';
-import davidBust from './assets/davidBust.png';
+import Content from './components/Content';
+import Header from './components/Header';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <h1>abefernan&apos;s site</h1>
-      <img src={davidBust} alt="David Bust" />
-      <h2>
-        Check out my game&nbsp;
-        <a href="https://abefernan.github.io/projects/wasspord/" target="_blank" rel="noopener noreferrer">
-          WassPord
-        </a>
-      </h2>
-    </div>
+    <>
+      <Header />
+      <Content />
+    </>
   );
 }
 
