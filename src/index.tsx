c056f6be73efda10ab6659a9a039667580b67466
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-07-18 15:20:18 +0200

    Changes codestyle and adds random theming

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import 'modern-css-reset';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
 import './index.css';
+import { applyRandomTheme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+applyRandomTheme();
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
